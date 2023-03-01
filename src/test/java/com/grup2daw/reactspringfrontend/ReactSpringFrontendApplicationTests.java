deaa657df89dd8b4c6f40441e333293e66a133ec
CristnaAT <alumno.668286@ies-azarquiel.es>
2023-03-01 12:21:00 +0100

    Initial commit

---
+++ src/test/java/com/grup2daw/reactspringfrontend/ReactSpringFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grup2daw.reactspringfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
